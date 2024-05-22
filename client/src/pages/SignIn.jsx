d4acb53c894b4cf5c4a1b4481505809475fbc83c
Jayabharath <bharathjothibasu2003@gmail.com>
2024-05-22 12:18:50 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,13 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
+import { useDispatch,useSelector } from "react-redux";
+import {signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice';
 
-
-export default function SignIn() {0
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage,setErrorMessage] = useState(null); 
-  const [loading,setLoading] = useState(false);
+  const  {loading,error: errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const Navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -14,11 +15,10 @@ export default function SignIn() {0
 const handleSubmit = async (e) =>{
   e.preventDefault();
   if ( !formData.email || !formData.password){
-    return setErrorMessage('please fill out all feilds');
+    return dispatch(signInFailure('please fill all the feilds'));
   }
   try {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch(signInStart());
     const res = await fetch('/api/auth/signin',{ 
       method:'POST',
       headers: {'Content-Type':'application/json'},
@@ -26,15 +26,15 @@ const handleSubmit = async (e) =>{
     });
     const data = await res.json();
     if (data.success === false) {
-      return setErrorMessage(data.message);
+      dispatch(signInFailure(data.message));
     }
-    setLoading(false);
+   
     if(res.ok){
+      dispatch(signInSuccess(data))
       Navigate('/')
     }
   } catch (error) {
-    setErrorMessage(error.message);
-    setLoading(false);
+    dispatch(signInFailure(error.message));
   }
 }; 
   return (
@@ -85,8 +85,7 @@ const handleSubmit = async (e) =>{
               <Alert className=" mt-5" color='failure'>
                 {errorMessage}
               </Alert>
-            ) 
-          }
+              )}
         </div>
       </div>
     </div>
