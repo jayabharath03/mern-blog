a6517ffdf8f7fabb37020cc39545f9f25a0f81a7
Jayabharath <bharathjothibasu2003@gmail.com>
2024-06-26 16:04:08 +0530

    add signout functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -59,4 +59,14 @@ export const deleteUser = async (req,res,next) =>{
     catch (error){
         next(error);
     }
+};
+
+export const signout = (req, res,next) => {
+    try{
+        res.clearCookie('acccess_token')
+        .status(200)
+        .json('User has been signed out');
+    } catch(error) {
+        next(error);
+    }
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -47,11 +47,16 @@ const userSlice = createSlice({
             state.loading=false;
             state.error = action.payload;
         },
+        signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        }
     },
 });
 
 export const { signInStart,signInSuccess,signInFailure,
     updateStart,updateSuccess,updateFailure,deleteUserStart,
-    deleteUserSuccess,deleteUserFailure,
+    deleteUserSuccess,deleteUserFailure,signoutSuccess,
 }= userSlice.actions;
 export default userSlice.reducer;
